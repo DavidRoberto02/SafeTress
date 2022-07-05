582e503a87126a36322c190316fc330a2442e599
David Roberto Acosta Saucedo <David.Sauced@Outlook.com>
2022-07-05 13:36:20 -0500

    fragments designs terminados.

--- app/src/main/java/app/safetress/application/view/MainActivity.kt
+++ app/src/main/java/app/safetress/application/view/MainActivity.kt
@@ -1,48 +1,48 @@
 package app.safetress.application.view
 
-import android.content.Intent
+
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Base64
+import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import app.safetress.application.*
-import app.safetress.application.utils.Constants
 import com.example.safetress.R
 import com.example.safetress.databinding.ActivityMainBinding
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var  mActiveFragment: Fragment
+    private lateinit var mActiveFragment: Fragment
     private lateinit var mFragmentManager: FragmentManager
 
     private lateinit var mAuthListener: FirebaseAuth.AuthStateListener
     private var mFirebaseAuth: FirebaseAuth? = null
 
-    private val authResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
-        if (result.resultCode == RESULT_OK){
-            Snackbar.make(binding.root, R.string.welcome_login_success, Snackbar.LENGTH_SHORT).show()
-        } else {
-            if (IdpResponse.fromResultIntent(result.data) == null){
-                finish()
+    private val authResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                Snackbar.make(binding.root, R.string.welcome_login_success, Snackbar.LENGTH_SHORT)
+                    .show()
+            } else {
+                if (IdpResponse.fromResultIntent(result.data) == null) {
+                    finish()
+                }
             }
         }
-    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,6 +52,9 @@ class MainActivity : AppCompatActivity() {
 
         setupAuth()
         setupBottomNav()
+        generateFBKey()
+
+        FacebookSdk.sdkInitialize(applicationContext)
 
     }
 
@@ -158,20 +161,38 @@ class MainActivity : AppCompatActivity() {
         mFirebaseAuth = FirebaseAuth.getInstance()
         mAuthListener = FirebaseAuth.AuthStateListener {
             val user = it.currentUser
-            if (user == null){
+            if (user == null) {
                 authResult.launch(
                     AuthUI.getInstance().createSignInIntentBuilder()
                         .setIsSmartLockEnabled(false)
                         .setAvailableProviders(
                             Arrays.asList(
+                                AuthUI.IdpConfig.FacebookBuilder().build(),
                                 AuthUI.IdpConfig.EmailBuilder().build(),
-                                AuthUI.IdpConfig.GoogleBuilder().build())
+                                AuthUI.IdpConfig.GoogleBuilder().build()
+                            )
                         )
                         .setTheme(R.style.LoginTheme)
                         .setLogo(R.drawable.safe_tress_img)
                         .build()
                 )
             }
+        }
+    }
+
+    fun generateFBKey(){
+        try {
+            val info = packageManager.getPackageInfo(
+                "app.safetress.application",
+                PackageManager.GET_SIGNATURES
+            )
+            for (signature in info.signatures) {
+                val md: MessageDigest = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+            }
+        } catch (e: PackageManager.NameNotFoundException) {
+        } catch (e: NoSuchAlgorithmException) {
         }
     }
 
@@ -184,7 +205,6 @@ class MainActivity : AppCompatActivity() {
         super.onPause()
         mFirebaseAuth?.removeAuthStateListener(mAuthListener)
     }
-
 
 
 }
